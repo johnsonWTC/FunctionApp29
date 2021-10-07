562dac939da27a81877d3a22ebe6f4aa392495c5
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-07 18:36:32 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp29
+{
+    class Startup : FunctionsStartup
+
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddScoped<>();
+            throw new NotImplementedException();
+        }
+    }
+}
